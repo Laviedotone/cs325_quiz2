2e2b17c6af6d8997aebe677e33dc58b192d93517
Laviedotone <laviedotone@gmail.com>
2026-02-02 07:23:40 -0600

    Operations added multiplication and division

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-#Mini Calculator
+# Mini Calculator
 
-##How to run the script
-###install python to run the calculator
-###Enter only numerical numbers for the inputs
+## How to run the script
+### install python to run the calculator
+### Enter only numerical numbers for the inputs
+
+## Updated new operations
+## Multipications and Division
--- calculator.py
+++ calculator.py
@@ -3,21 +3,37 @@ def calculator():
     print("Choose an operation:")
     print("1. Addition")
     print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
 
-    choice = input("Enter 1 or 2: ")
+    choice = input("Enter 1, 2, 3, or 4: ")
 
-    a = float(input("Enter the first number: "))
+    a = float(input("Enter the first number: ")) #float allows decimal/scientific numbers
     b = float(input("Enter the second number: "))
 
 
     if choice == '1':
         result = a + b
         print("Result:" , result)
+
     elif choice == '2':
         result = a - b
         print("Result:", result)
-    else: 
-        print("invalid choice")
+
+    elif choice == '3':
+        result = a * b
+        print("Result:", result)
+    
+    elif choice == '4':
+        if b == 0:
+            print("Error: Cannot divide by zero.") # would get an undefined number if zero is input for b
+        else:
+            result = a / b
+            print("Result:", result)
+
+    else:
+        print("Invalid input")
+        
 
 calculator()
 
